12a04f2e9de5b67b540ce5e6f8cfcfc67d9bdbf2
darrenMabbayad <mabbayadd@gmail.com>
2020-04-14 08:33:30 -0400

    project info and images updated

--- src/layouts/mobile/pages/ProjectPage.js
+++ src/layouts/mobile/pages/ProjectPage.js
@@ -36,17 +36,70 @@ function ProjectPage({projects}) {
             <div className='project-page-desktop-full'>
                 <p className='project-page-desktop-full-text'>{thisProject.context}</p>
                 <p className='project-page-desktop-full-text'>{thisProject.explanationOne}</p>
-                <div className='project-page-desktop-full-image'>
-                        <img src={thisProject.extraImg_1} alt=''/>                      
-                </div>
+                {
+                    thisProject.extraImg_1 ? 
+                        <div className='project-page-desktop-full-image'>
+                                <img src={thisProject.extraImg_1} alt=''/>                      
+                        </div> : null
+                }
+
                 <p className='project-page-desktop-full-text'>{thisProject.explanationTwo}</p>
+                {
+                    thisProject.extraImg_1_odd ? 
+                        <div className='project-page-desktop-full-image'>
+                                <img src={thisProject.extraImg_1_odd} alt=''/>                      
+                        </div> : null
+                }
                 <p className='project-page-desktop-full-text'>{thisProject.explanationThree}</p>
+
+                {
+                    thisProject.extraImg_2 ? 
+                        <div className='project-page-desktop-full-image'>
+                                <img src={thisProject.extraImg_2} alt=''/>                      
+                        </div> : null
+                }
                 <p className='project-page-desktop-full-text'>{thisProject.explanationFour}</p>
+
+                {
+                    thisProject.extraImg_2_odd ? 
+                        <div className='project-page-desktop-full-image'>
+                                <img src={thisProject.extraImg_2_odd} alt=''/>                      
+                        </div> : null
+                }
                 <p className='project-page-desktop-full-text'>{thisProject.explanationFive}</p>
+
+                {
+                    thisProject.extraImg_3 ? 
+                        <div className='project-page-desktop-full-image'>
+                                <img src={thisProject.extraImg_3} alt=''/>                      
+                        </div> : null
+                }
                 {thisProject.explanationSix ? <p className='project-page-desktop-full-text'>{thisProject.explanationSix}</p> : null}
+
+                {
+                    thisProject.extraImg_3_odd ? 
+                        <div className='project-page-desktop-full-image'>
+                                <img src={thisProject.extraImg_3_odd} alt=''/>                      
+                        </div> : null
+                }
                 {thisProject.explanationSeven ? <p className='project-page-desktop-full-text'>{thisProject.explanationSeven}</p> : null}
+
+                {
+                    thisProject.extraImg_4 ? 
+                        <div className='project-page-desktop-full-image'>
+                                <img src={thisProject.extraImg_4} alt=''/>                      
+                        </div> : null
+                }
                 {thisProject.explanationEight ? <p className='project-page-desktop-full-text'>{thisProject.explanationEight}</p> : null}
+
+                {
+                    thisProject.extraImg_4_odd ? 
+                        <div className='project-page-desktop-full-image'>
+                                <img src={thisProject.extraImg_4_odd} alt=''/>                      
+                        </div> : null
+                }
                 {thisProject.explanationNine ? <p className='project-page-desktop-full-text'>{thisProject.explanationNine}</p> : null}
+                
                 {thisProject.explanationTen ? <p className='project-page-desktop-full-text'>{thisProject.explanationTen}</p> : null}
             </div>
         </div>
