b9375aa414556f390809d9941fdb98803a01059c
darrenMabbayad <mabbayadd@gmail.com>
2020-09-14 17:54:36 -0400

    mostly desktop layout changes

--- src/layouts/desktop/components/Project.js
+++ src/layouts/desktop/components/Project.js
@@ -1,54 +1,52 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 function Project({ project }) {
   return (
-    <>
-      <div className='projects-desktop-image'>
-        <img src={project.image} alt='' />
-      </div>
-      <div className='projects-desktop-text'>
-        <div className='projects-desktop-text-info'>
-          <h1 className='projects-desktop-text-info-title'>{project.title}</h1>
-          <p className='projects-desktop-text-info-descr'>
-            {project.description}
-          </p>
-          <div>
-            <Link className='learn-more-btn' to={`/${project.name}`}>
-              Learn More
-            </Link>
+    <div className="projects-desktop-card">
+      <div className="projects-desktop-card-info">
+        <h1 className="projects-desktop-card-info-title">{project.title}</h1>
+        <div className="projects-desktop-image">
+          <img src={project.image} alt="" />
+        </div>
+        <p className="projects-desktop-card-info-descr">
+          {project.description}
+        </p>
+        <div className="projects-desktop-card-info-links">
+          <Link className="learn-more-btn" to={`/${project.name}`}>
+            Learn More
+          </Link>
+          <a
+            className="learn-more-btn togithub"
+            href={project.githubRepo}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Github Repo
+          </a>
+          {project.deployLink && (
             <a
-              className='learn-more-btn togithub'
-              href={project.githubRepo}
-              target='_blank'
-              rel='noopener noreferrer'
+              className="learn-more-btn togithub"
+              href={project.deployLink}
+              target="_blank"
+              rel="noopener noreferrer"
             >
-              Github Repo
+              Live Link
             </a>
-            {project.deployLink && (
-              <a
-                className='learn-more-btn togithub'
-                href={project.deployLink}
-                target='_blank'
-                rel='noopener noreferrer'
-              >
-                Live Link
-              </a>
-            )}
-          </div>
-        </div>
-        <div className='projects-desktop-text-list'>
-          <h3 className='projects-desktop-text-list-heading'>
-            Technologies Used:
-          </h3>
-          <ul>
-            {project.technologies.map((technology, i) => (
-              <li key={i}>{technology}</li>
-            ))}
-          </ul>
+          )}
         </div>
       </div>
-    </>
+      <div className="projects-desktop-card-list">
+        <h3 className="projects-desktop-card-list-heading">
+          Technologies Used:
+        </h3>
+        <ul>
+          {project.technologies.map((technology, i) => (
+            <li key={i}>{technology}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
   );
 }
 
