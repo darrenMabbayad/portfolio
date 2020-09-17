e4c915927e58f77788c110513770463fe8494e63
darrenMabbayad <mabbayadd@gmail.com>
2020-09-17 12:31:49 -0400

    mobile css changes

--- src/layouts/mobile/components/Project.js
+++ src/layouts/mobile/components/Project.js
@@ -2,26 +2,52 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 function Project({ project }) {
-  const { id, title, name } = project;
   return (
-    <div>
-      <Link to={`/${name}`}>
-        <div
-          style={{
-            backgroundImage: `url(${project.image})`,
-            backgroundSize: "contain",
-            backgroundRepeat: "no-repeat",
-            backgroundPosition: "center",
-          }}
-          id={`project-mobile-${id}`}
-          className="project-mobile"
-        >
-          <div className="project-mobile-title">
-            <h3 className="project-mobile-title-text">{title}</h3>
+    <div className="projects-mobile">
+      <div className="projects-mobile-card">
+        <div className="projects-mobile-card-info">
+          <h1 className="projects-mobile-card-info-title">{project.title}</h1>
+          <div className="projects-mobile-image">
+            <img src={project.image} alt="" />
+          </div>
+          <p className="projects-mobile-card-info-descr">
+            {project.description}
+          </p>
+          <div className="projects-mobile-card-info-links">
+            <Link className="learn-more-btn" to={`/${project.name}`}>
+              Learn More
+            </Link>
+            <a
+              className="learn-more-btn togithub"
+              href={project.githubRepo}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Github Repo
+            </a>
+            {project.deployLink && (
+              <a
+                className="learn-more-btn togithub"
+                href={project.deployLink}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Live Link
+              </a>
+            )}
           </div>
         </div>
-      </Link>
-      <p className="project-mobile-description">{project.description}</p>
+        <div className="projects-mobile-card-list">
+          <h3 className="projects-mobile-card-list-heading">
+            Technologies Used:
+          </h3>
+          <ul>
+            {project.technologies.map((technology, i) => (
+              <li key={i}>{technology}</li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
