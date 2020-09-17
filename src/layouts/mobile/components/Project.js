eb8af8d50a69f9693b4734e0e1414597fa2b1924
darrenMabbayad <mabbayadd@gmail.com>
2020-09-17 12:44:49 -0400

    fixed gap between projects in mobile

--- src/layouts/mobile/components/Project.js
+++ src/layouts/mobile/components/Project.js
@@ -3,49 +3,51 @@ import { Link } from "react-router-dom";
 
 function Project({ project }) {
   return (
-    <div className="projects-mobile">
-      <div className="projects-mobile-card">
-        <div className="projects-mobile-card-info">
-          <h1 className="projects-mobile-card-info-title">{project.title}</h1>
-          <div className="projects-mobile-image">
-            <img src={project.image} alt="" />
-          </div>
-          <p className="projects-mobile-card-info-descr">
-            {project.description}
-          </p>
-          <div className="projects-mobile-card-info-links">
-            <Link className="learn-more-btn" to={`/${project.name}`}>
-              Learn More
-            </Link>
-            <a
-              className="learn-more-btn togithub"
-              href={project.githubRepo}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Github Repo
-            </a>
-            {project.deployLink && (
+    <div>
+      <div className="projects-mobile">
+        <div className="projects-mobile-card">
+          <div className="projects-mobile-card-info">
+            <h1 className="projects-mobile-card-info-title">{project.title}</h1>
+            <div className="projects-mobile-image">
+              <img src={project.image} alt="" />
+            </div>
+            <p className="projects-mobile-card-info-descr">
+              {project.description}
+            </p>
+            <div className="projects-mobile-card-info-links">
+              <Link className="learn-more-btn" to={`/${project.name}`}>
+                Learn More
+              </Link>
               <a
                 className="learn-more-btn togithub"
-                href={project.deployLink}
+                href={project.githubRepo}
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                Live Link
+                Github Repo
               </a>
-            )}
+              {project.deployLink && (
+                <a
+                  className="learn-more-btn togithub"
+                  href={project.deployLink}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Live Link
+                </a>
+              )}
+            </div>
           </div>
-        </div>
-        <div className="projects-mobile-card-list">
-          <h3 className="projects-mobile-card-list-heading">
-            Technologies Used:
-          </h3>
-          <ul>
-            {project.technologies.map((technology, i) => (
-              <li key={i}>{technology}</li>
-            ))}
-          </ul>
+          <div className="projects-mobile-card-list">
+            <h3 className="projects-mobile-card-list-heading">
+              Technologies Used:
+            </h3>
+            <ul>
+              {project.technologies.map((technology, i) => (
+                <li key={i}>{technology}</li>
+              ))}
+            </ul>
+          </div>
         </div>
       </div>
     </div>
